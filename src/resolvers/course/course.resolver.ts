1ee618bd97b2f80dfb512c7efa29505a54073f00
Lizard Morrow <lizardmorrow@gmail.com>
2024-11-10 15:02:41 +0000

    feat: course resolver
    - update get courses service ot take optional arguments

--- src/resolvers/course/course.resolver.ts
+++ src/resolvers/course/course.resolver.ts
@@ -2,12 +2,23 @@ import {Arg, Int, Query, Resolver} from "type-graphql";
 import {Course} from "./types";
 import {getCoursesFromDbService} from "./get-courses-from-db-service/get-courses-from-db-service";
 import {getCourseByIdService} from "./get-course-by-id-service/get-course-by-id-service";
+import {SortOrder} from "../../utils/enums/sort-order";
 
 @Resolver()
 export class CourseResolver {
     @Query(() => [Course])
-    async getCourses(): Promise<Course[]> {
-        return getCoursesFromDbService();
+    async getCourses(
+        @Arg('limit', () => Int, {
+            nullable: true,
+            defaultValue: 50,
+        }) limit?: number,
+        @Arg('offset', () => Int, {
+            nullable: true,
+            defaultValue: 0
+        }) offset?: number,
+        @Arg('sortOrder', () => String, { nullable: true }) sortOrder?: SortOrder
+    ): Promise<Course[]> {
+        return getCoursesFromDbService(limit, sortOrder, offset);
     }
 
     @Query(() => Course)
--- src/resolvers/course/get-courses-from-db-service/get-courses-from-db-service.ts
+++ src/resolvers/course/get-courses-from-db-service/get-courses-from-db-service.ts
@@ -2,16 +2,20 @@ import {Course} from "../types";
 import {db} from "../../../lib/db";
 import {logger} from "../../../lib/logger";
 import {selectColumns} from "../common/select-columns";
+import {SortOrder} from "../../../utils/enums/sort-order";
 
 const log = logger.child({
     service: 'getCoursesFromDbService'
 });
 
-export async function getCoursesFromDbService(): Promise<Course[]> {
+export async function getCoursesFromDbService(limit: number, sortOrder: SortOrder, offset: number = 0): Promise<Course[]> {
     log.info('getCoursesFromDbService - INIT');
 
     const result = await db.select(selectColumns)
-        .from('courses');
+        .from('courses')
+        .orderBy('title', sortOrder)
+        .limit(limit)
+        .offset(offset);
 
     log.info('getCoursesFromDbService - END');
 
